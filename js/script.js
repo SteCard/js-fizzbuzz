fed9a2264b65e72434dbc72275bf99cadcc7de6a
SteCard <stefanocardillo14@gmail.com>
2023-05-21 21:19:08 +0200

    FizzBuzz working

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+for (let i = 1; i <= 100; i++) {
+
+    let output = "";
+
+    if (i % 3 === 0) {output += "Fizz"; }
+    if (i % 5 === 0) {output += "Buzz"; }
+
+    if (output === "") { output = i; }
+
+    console.log(output);
+    
+}
